4d8a194ce4ae96a281a6d1144773a8f3e049481b
Ben von Huenerbein <ben.v.h@hotmail.com>
2024-09-20 14:25:51 +0200

    Change config from normal to dataclasses

--- src/elise/config.py
+++ src/elise/config.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass
 from typing import Any, Dict
 
 import toml
@@ -13,55 +14,55 @@ class Config:
         return self._config.get(section, {})
 
 
+@dataclass
 class NetworkConfig:
-    def __init__(self, config: Dict[str, Any]):
-        self.num_lat: int = config.get("num_lat", 50)
-        self.num_vis: int = config.get("num_vis", 13)
+    num_lat: int = 50
+    num_vis: int = 13
 
 
+@dataclass
 class WeightConfig:
-    def __init__(self, config: Dict[str, Any]):
-        self.p: float = config.get("p", 0.5)
-        self.q: float = config.get("q", 0.3)
-        self.p0: float = config.get("p0", 0.1)
-        self.sparsity: float = config.get("sparsity", 0.3)
-        self.W_out_out: list = config.get("W_out_out", [0.0, 0.5])
-        self.W_out_lat: list = config.get("W_out_lat", [0.0, 0.5])
-        self.W_lat_lat: list = config.get("W_lat_lat", [0.0, 0.5])
-        self.W_lat_out: list = config.get("W_lat_out", [0.0, 0.5])
-        self.d_som_min: int = config.get("d_som_min", 5)
-        self.d_som_max: int = config.get("d_som_max", 15)
-        self.d_den_min: int = config.get("d_den_min", 5)
-        self.d_den_max: int = config.get("d_den_max", 15)
+    p: float = 0.5
+    q: float = 0.3
+    p0: float = 0.1
+    sparsity: float = 0.3
+    W_out_out: list = [0.0, 0.5]
+    W_out_lat: list = [0.0, 0.5]
+    W_lat_lat: list = [0.0, 0.5]
+    W_lat_out: list = [0.0, 0.5]
+    d_som_min: int = 5
+    d_som_max: int = 15
+    d_den_min: int = 5
+    d_den_max: int = 15
 
 
+@dataclass
 class SimulationConfig:
-    def __init__(self, config: Dict[str, Any]):
-        self.input: str = config.get("input", "test")
-        self.dt: float = config.get("dt", 0.01)
-        self.training_cycles: float = config.get("training_cycles", 10e4)
-        self.replay_cycles: int = config.get("replay_cycles", 100)
-        self.validation_interval: int = config.get("validation_interval", 20)
-        self.eta_out: float = config.get("eta_out", 10e-4)
-        self.eta_lat: float = config.get("eta_lat", 10e-3)
+    input: str = "test"
+    dt: float = 0.01
+    training_cycles: float = 10e4
+    replay_cycles: int = 100
+    validation_interval: int = 20
+    eta_out: float = 10e-4
+    eta_lat: float = 10e-3
 
 
+@dataclass
 class NeuronConfig:
-    def __init__(self, config: Dict[str, Any]):
-        self.C_v: float = config.get("C_v", 1.0)
-        self.C_u: float = config.get("C_u", 1.0)
-        self.E_l: float = config.get("E_l", -70.0)
-        self.E_exc: float = config.get("E_e", 0.0)
-        self.E_inh: float = config.get("E_i", -75.0)
-        self.g_lat: float = config.get("g_l", 0.1)
-        self.g_den: float = config.get("g_d", 2.0)
-        self.g_exc: float = config.get("g_e", 0.3)
-        self.g_inh: float = config.get("g_i", 6.0)
-        self.a: float = config.get("a", 0.3)
-        self.b: float = config.get("b", -58.0)
-        self.d_den: list = config.get("d_d", [5, 15])
-        self.d_som: list = config.get("d_s", [5, 15])
-        self.d_int: int = config.get("d_t", 25)
+    C_v: float = 1.0
+    C_u: float = 1.0
+    E_l: float = -70.0
+    E_exc: float = 0.0
+    E_inh: float = -75.0
+    g_lat: float = 0.1
+    g_den: float = 2.0
+    g_exc: float = 0.3
+    g_inh: float = 6.0
+    a: float = 0.3
+    b: float = -58.0
+    d_den: list = [5, 15]
+    d_som: list = [5, 15]
+    d_int: int = 25
 
 
 class FullConfig:
