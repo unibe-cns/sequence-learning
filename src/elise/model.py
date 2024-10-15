13f94e088736852c250fe0108008116ea0db74b4
Timo Gierlich <timo@eltima-electronic.de>
2024-10-15 18:07:37 +0200

    Implement model equations and tests (#30)
    Does not include the buffer code, i.e. the access of the delayed rates
    and the writing to the buffer.
    
    - use a functional approach
    - use numba
    - employ strictly test-driven design
    
    Important: Depends on BUGFIXES #26 and #29

--- src/elise/model.py
+++ src/elise/model.py
@@ -2,11 +2,16 @@
 from abc import ABC, abstractmethod
 from typing import Tuple
 
+import numba
 import numpy as np
 import numpy.typing as npt
 
 from .config import NetworkConfig, NeuronConfig, WeightConfig
 from .rate_buffer import Buffer
+
+
+# disable numba jit for debugging etc.
+numba.config.DISABLE_JIT = False
 
 
 class Weights(ABC):
@@ -154,9 +159,6 @@ class Neurons:
         self.I_som = np.zeros(num_neurons)
         self.rate_buffer = rate_buffer(num_neurons)
 
-    def phi(self, v):
-        return 0.5 * (1 + np.tanh(v / 2))
-
 
 class Network:
     def __init__(
@@ -187,3 +189,240 @@ class Network:
     def update(self):
         # Implement update logic here
         pass
+
+
+#####################################
+# equations of the dynamical system #
+#####################################
+
+
+@numba.njit
+def eq_phi(u: npt.NDArray | float, a: float, b: float) -> npt.NDArray:
+    """Activation function."""
+    return 1.0 / (1 + np.exp(a * (b - u)))
+
+
+@numba.njit()
+def eq_i_den(r_delayed: npt.NDArray, w_den: npt.NDArray) -> npt.NDArray:
+    """Dendritic synaptic current."""
+    i_den = np.dot(w_den, r_delayed)
+    return i_den
+
+
+@numba.vectorize()
+def eq_syn_cond(r_delay: float, g_0: float, phi_el: float) -> float:
+    """Calc the synaptic conductances for the somatic weights."""
+    if r_delay > phi_el:
+        return r_delay * g_0
+    else:
+        return phi_el * g_0
+
+
+@numba.njit()
+def eq_rescale_v(v: npt.NDArray, g_l: float, g_den: float, E_l: float) -> npt.NDArray:
+    """Rescale dendritic voltage to somatic for plasticity."""
+    return (g_l * E_l + g_den * v) / (g_l + g_den)
+
+
+@numba.njit()
+def eq_drbar_dt(
+    r_bar: npt.NDArray, r_delay: npt.NDArray, g_l: float, g_den: float
+) -> npt.NDArray:
+    """Diff. eq. for the lowpass filtered rate used in the plasticity."""
+    return -g_l * r_bar + (g_l * g_den) / (g_l + g_den) * r_delay
+
+
+@numba.njit()
+def eq_i_som(
+    w_som: npt.NDArray,
+    g_exc: npt.NDArray,
+    g_inh: npt.NDArray,
+    u: npt.NDArray,
+    E_exc: float,
+    E_inh: float,
+) -> npt.NDArray:
+    """Calculate the somatic input current w/o teaching synapses."""
+    i_som = np.dot(w_som, g_exc) * (E_exc - u) + np.dot(w_som, g_inh) * (E_inh - u)
+    return i_som
+
+
+@numba.njit()
+def eq_cond_exc_inp(
+    u_inp: npt.NDArray, lam: float, g_l: float, g_den: float, E_exc: float, E_inh: float
+) -> npt.NDArray:
+    """Calc the axcitatory conductances for the input."""
+    g_exc = lam * (g_l + g_den) * (E_inh - u_inp) / ((1 - lam) * (E_inh - E_exc))
+    return g_exc
+
+
+@numba.njit()
+def eq_cond_inh_inp(
+    u_inp: npt.NDArray, lam: float, g_l: float, g_den: float, E_exc: float, E_inh: float
+) -> npt.NDArray:
+    """Calc the excitatory conductances for the input."""
+    g_exc = lam * (g_l + g_den) * (u_inp - E_exc) / ((1 - lam) * (E_inh - E_exc))
+    return g_exc
+
+
+@numba.njit()
+def eq_i_inp(
+    g_exc_inp: npt.NDArray,
+    g_inh_inp: npt.NDArray,
+    u: npt.NDArray,
+    E_exc: float,
+    E_inh: float,
+) -> npt.NDArray:
+    """Calc the synaptic current of the input synapses.
+
+    This is added only to the visible neurons.
+    """
+    i_inp = g_exc_inp * (E_exc - u) + g_inh_inp * (E_inh - u)
+    return i_inp
+
+
+@numba.njit()
+def eq_dvdt(
+    v: npt.NDArray, i_den: npt.NDArray, C_m: float, g_l: float, E_l: float
+) -> npt.NDArray:
+    """Diff. eq. for the dendritic potential v."""
+    dvdt = (-g_l * (v - E_l) + i_den) / C_m
+    return dvdt
+
+
+@numba.njit()
+def eq_dudt(
+    u: npt.NDArray,
+    v: npt.NDArray,
+    i_som: npt.NDArray,
+    C_m: float,
+    g_l: float,
+    g_den: float,
+    E_l: float,
+) -> npt.NDArray:
+    """Diff. eq. for the somatic potential."""
+    dudt = (-g_l * (u - E_l) + g_den * (v - u) + i_som) / C_m
+    return dudt
+
+
+@numba.njit()
+def eq_dwdt(phi_u: npt.NDArray, phi_v: npt.NDArray, r_bar: npt.NDArray) -> npt.NDArray:
+    """Urbanczik-Senn plasiticity rule."""
+    dwdt = np.outer(phi_u - phi_v, r_bar)
+    return dwdt
+
+
+@numba.njit()
+def total_diff_eq(
+    u: npt.NDArray,
+    v: npt.NDArray,
+    w_den: npt.NDArray,
+    r_bar: npt.NDArray,
+    r_den: npt.NDArray,
+    r_exc: npt.NDArray,
+    r_inh: npt.NDArray,
+    u_inp: npt.NDArray,
+    w_som: npt.NDArray,
+    C_u: float,
+    C_v: float,
+    g_l: float,
+    g_den: float,
+    g_exc_0: float,
+    g_inh_0: float,
+    E_l: float,
+    E_exc: float,
+    E_inh: float,
+    a: float,
+    b: float,
+    lam: float,
+) -> Tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
+    """
+    Calculate the total differential equations for a neural network model.
+
+    This function computes the time derivatives of membrane potentials, synaptic
+    weights, and firing rates for a network of neurons with dendritic and somatic
+    compartments.
+
+    :param u: Somatic membrane potentials, dynamic model variable (1-dim vector of size N)  # noqa
+    :type u: numpy.ndarray
+    :param v: Dendritic membrane potentials, dynamic model variable (1-dim vector of size N)  # noqa
+    :type v: numpy.ndarray
+    :param w_den: Dendritic synaptic weights, dynamic model variable (2-dim matrix of size NxN)  # noqa
+    :type w_den: numpy.ndarray
+    :param r_bar: Average firing rates, dynamic model variable (1-dim vector of size N)  # noqa
+    :type r_bar: numpy.ndarray
+    :param r_den: Delayed dendritic firing rates, instanteous variable (1-dim vector of size N)  # noqa
+    :type r_den: numpy.ndarray
+    :param r_exc: delayed somatic excitatory rates, instanteous variable (1-dim vector of size N)  # noqa
+    :type r_exc: numpy.ndarray
+    :param r_inh: delayed somatic inhibitory rates, instanteous variable (1-dim vector of size N)  # noqa
+    :type r_inh: numpy.ndarray
+    :param u_inp: Input membrane potentials, instanteous variable (1-dim vector of size N_visible)  # noqa
+    :type u_inp: numpy.ndarray
+    :param w_som: Sparse somatic synaptic weights, fixed model parameter (2-dim matrix of size NxN)  # noqa
+    :type w_som: numpy.ndarray
+    :param C_u: Somatic membrane capacitance, fixed model parameter
+    :type C_u: float
+    :param C_v: Dendritic membrane capacitance, fixed model parameter
+    :type C_v: float
+    :param g_l: Leak conductance, fixed model parameter
+    :type g_l: float
+    :param g_den: Dendritic coupling conductance, fixed model parameter
+    :type g_den: float
+    :param g_exc_0: Baseline excitatory conductance, fixed model parameter
+    :type g_exc_0: float
+    :param g_inh_0: Baseline inhibitory conductance, fixed model parameter
+    :type g_inh_0: float
+    :param E_l: Leak reversal potential, fixed model parameter
+    :type E_l: float
+    :param E_exc: Excitatory reversal potential, fixed model parameter
+    :type E_exc: float
+    :param E_inh: Inhibitory reversal potential, fixed model parameter
+    :type E_inh: float
+    :param a: Activation function parameter, fixed model parameter
+    :type a: float
+    :param b: Activation function parameter, fixed model parameter
+    :type b: float
+    :param lam: Input scaling factor, fixed model parameter
+    :type lam: float
+
+    :return: Tuple containing time derivatives of u, v, w, and r_bar
+    :rtype: Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray]
+
+    This function implements various equations for neural dynamics, including:
+    - Dendritic and somatic currents
+    - Synaptic conductances
+    - Membrane potential dynamics
+    - Firing rate dynamics
+    - Synaptic plasticity
+
+    The function uses helper functions (not shown here) to compute individual components
+    of the model, such as eq_i_den, eq_syn_cond, eq_i_som, etc.
+    """
+    phi_el = eq_phi(E_l, a, b)
+    num_vis = len(u_inp)
+
+    # somato-dendritic connections
+    i_den = eq_i_den(r_den, w_den)  # eq. 8
+
+    # somato-somatic conentions
+    g_exc = eq_syn_cond(r_exc, g_exc_0, phi_el)  # eq. 10
+    g_inh = eq_syn_cond(r_inh, g_inh_0, phi_el)  # eq. 11
+    i_som = eq_i_som(w_som, g_exc, g_inh, u, E_exc, E_inh)
+    # input to visible layer
+    g_exc_inp = eq_cond_exc_inp(u_inp, lam, g_l, g_den, E_exc, E_inh)
+    g_inh_inp = eq_cond_inh_inp(u_inp, lam, g_l, g_den, E_exc, E_inh)
+    i_som_inp = eq_i_inp(g_exc_inp, g_inh_inp, u[:num_vis], E_exc, E_inh)
+    # total somatic input current:
+    i_som[:num_vis] += i_som_inp
+
+    # differential equations:
+    dvdt = eq_dvdt(v, i_den, C_v, g_l, E_l)
+    dudt = eq_dudt(u, v, i_som, C_u, g_l, g_den, E_l)
+    dr_bar_dt = eq_drbar_dt(r_bar, r_den, g_l, g_den)
+    # plasticity
+    phi_u = eq_phi(u, a, b)
+    v_rescaled = eq_rescale_v(v, g_l, g_den, E_l)
+    phi_v = eq_phi(v_rescaled, a, b)
+    dwdt = eq_dwdt(phi_u, phi_v, r_bar)
+
+    return dudt, dvdt, dwdt, dr_bar_dt
---
+++ tests/test_model_equations.py
@@ -0,0 +1,431 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+
+from elise.config import NetworkConfig, NeuronConfig, WeightConfig
+from elise.model import (
+    eq_cond_exc_inp,
+    eq_cond_inh_inp,
+    eq_drbar_dt,
+    eq_dudt,
+    eq_dvdt,
+    eq_dwdt,
+    eq_i_den,
+    eq_i_inp,
+    eq_i_som,
+    eq_phi,
+    eq_rescale_v,
+    eq_syn_cond,
+    total_diff_eq,
+)
+
+
+@pytest.fixture
+def std_nrn():
+    return NeuronConfig()
+
+
+@pytest.fixture
+def std_wgt():
+    return WeightConfig()
+
+
+@pytest.fixture
+def std_nw():
+    return NetworkConfig(num_vis=2, num_lat=3)
+
+
+@pytest.fixture
+def sample_u():
+    return np.array([-70.0, -65.0, -60.0, -55.0, -50.0])
+
+
+@pytest.fixture
+def sample_v():
+    return np.array([-71.0, -66.0, -61.0, -56.0, -51.0])
+
+
+@pytest.fixture
+def sample_inp(std_nrn):
+    # target pattern z = [0, 1]
+    return np.array([std_nrn.E_l + 0.0, std_nrn.E_l + 20])
+
+
+@pytest.fixture
+def sample_r(sample_u, std_nrn):
+    return eq_phi(sample_u, std_nrn.a, std_nrn.b)
+
+
+@pytest.fixture
+def sample_r_delay_den(sample_r):
+    return sample_r * 0.99
+
+
+@pytest.fixture
+def sample_r_delay_exc(sample_r):
+    return sample_r * 0.96
+
+
+@pytest.fixture
+def sample_r_delay_inh(sample_r):
+    return sample_r * 0.92
+
+
+@pytest.fixture
+def sample_r_bar(sample_r):
+    return sample_r * 0.94
+
+
+@pytest.fixture
+def sample_g_exc(sample_r, std_nrn):
+    phi_el = eq_phi(std_nrn.E_l, std_nrn.a, std_nrn.b)
+    g_exc = eq_syn_cond(sample_r, std_nrn.g_exc, phi_el)
+    return g_exc
+
+
+@pytest.fixture
+def sample_g_inh(sample_r, std_nrn):
+    phi_el = eq_phi(std_nrn.E_l, std_nrn.a, std_nrn.b)
+    g_inh = eq_syn_cond(sample_r * 0.95, std_nrn.g_inh, phi_el)
+    return g_inh
+
+
+@pytest.fixture
+def sample_g_exc_inp(sample_inp, std_nrn):
+    res = eq_cond_exc_inp(
+        sample_inp,
+        std_nrn.lam,
+        std_nrn.g_l,
+        std_nrn.g_den,
+        std_nrn.E_exc,
+        std_nrn.E_inh,
+    )
+    return res
+
+
+@pytest.fixture
+def sample_g_inh_inp(sample_inp, std_nrn):
+    res = eq_cond_inh_inp(
+        sample_inp,
+        std_nrn.lam,
+        std_nrn.g_l,
+        std_nrn.g_den,
+        std_nrn.E_exc,
+        std_nrn.E_inh,
+    )
+    return res
+
+
+@pytest.fixture
+def sample_W_den(std_nw):
+    """Sets up the dendritic weight matrix.
+
+    Not produces by the actual weight creation class.
+    """
+    size = std_nw.num_vis + std_nw.num_lat
+    w = np.linspace(0.0, 0.5, size**2).reshape(size, size)
+    np.fill_diagonal(w, 0.0)
+    return w
+
+
+@pytest.fixture
+def sample_W_som():
+    w = np.array(
+        [
+            [0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 1.0, 0.0],
+            [1.0, 0.0, 0.0, 0.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0, 0.0],
+        ]
+    )
+    return w
+
+
+@pytest.fixture
+def sample_i_den(sample_r, sample_W_den):
+    res = eq_i_den(sample_r, sample_W_den)
+    return res
+
+
+@pytest.fixture
+def sample_i_som(sample_W_som, sample_g_exc, sample_g_inh, sample_u, std_nrn):
+    res = eq_i_som(
+        sample_W_som, sample_g_exc, sample_g_inh, sample_u, std_nrn.E_exc, std_nrn.E_inh
+    )
+    return res
+
+
+@pytest.fixture
+def sample_derivatives(
+    sample_u,
+    sample_v,
+    sample_r_delay_den,
+    sample_r_delay_exc,
+    sample_r_delay_inh,
+    sample_r_bar,
+    sample_inp,
+    sample_W_som,
+    sample_W_den,
+    std_nrn,
+    std_nw,
+):
+    """reference implementation for total_diff_eq.
+
+    No guarantee that this is actually correct...
+    However, if this and total_diff_eq yield the same results, the probabilty for
+    for correctness is higher.
+    Should be quite handy to have if we change implementation.
+    """
+    i_den = eq_i_den(sample_r_delay_den, sample_W_den)
+
+    phi_el = eq_phi(std_nrn.E_l, std_nrn.a, std_nrn.b)
+
+    g_exc = eq_syn_cond(sample_r_delay_exc, std_nrn.g_exc, phi_el)
+    g_inh = eq_syn_cond(sample_r_delay_inh, std_nrn.g_inh, phi_el)
+
+    i_som = eq_i_som(sample_W_som, g_exc, g_inh, sample_u, std_nrn.E_exc, std_nrn.E_inh)
+    g_exc_inp = eq_cond_exc_inp(
+        sample_inp,
+        std_nrn.lam,
+        std_nrn.g_l,
+        std_nrn.g_den,
+        std_nrn.E_exc,
+        std_nrn.E_inh,
+    )
+    g_inh_inp = eq_cond_inh_inp(
+        sample_inp,
+        std_nrn.lam,
+        std_nrn.g_l,
+        std_nrn.g_den,
+        std_nrn.E_exc,
+        std_nrn.E_inh,
+    )
+    i_som_inp = eq_i_inp(
+        g_exc_inp, g_inh_inp, sample_u[: std_nw.num_vis], std_nrn.E_exc, std_nrn.E_inh
+    )
+    i_som[: std_nw.num_vis] += i_som_inp
+
+    dvdt = eq_dvdt(sample_v, i_den, std_nrn.C_v, std_nrn.g_l, std_nrn.E_l)
+    dudt = eq_dudt(
+        sample_u, sample_v, i_som, std_nrn.C_u, std_nrn.g_l, std_nrn.g_den, std_nrn.E_l
+    )
+
+    # input to plasticity rule
+    phi_u = eq_phi(sample_u, std_nrn.a, std_nrn.b)
+    v_rescaled = eq_rescale_v(sample_v, std_nrn.g_l, std_nrn.g_den, std_nrn.E_l)
+    phi_v_rescaled = eq_phi(v_rescaled, std_nrn.a, std_nrn.b)
+    dwdt = eq_dwdt(phi_u, phi_v_rescaled, sample_r_bar)
+    dr_bar_dt = eq_drbar_dt(
+        sample_r_bar, sample_r_delay_den, std_nrn.g_l, std_nrn.g_den
+    )
+
+    return dudt, dvdt, dwdt, dr_bar_dt
+
+
+def test_activation_function(sample_u, std_nrn):
+    """Tests the activation function"""
+    res = eq_phi(sample_u, std_nrn.a, std_nrn.b)
+    expected = np.array([0.02659699, 0.10909682, 0.35434369, 0.7109495, 0.9168273])
+    assert_allclose(res, expected, rtol=1e-6)
+
+
+def test_i_den(sample_r, sample_W_den, sample_i_den):
+    """Tests the Dentritic synaptic input current.
+
+    I_den = W_den @ r_delayed
+    """
+    expected = np.array([0.13787379, 0.34484235, 0.49049918, 0.5330847, 0.56188277])
+    assert_allclose(sample_i_den, expected, rtol=1e-6)
+
+
+def test_syn_cond(sample_r, std_nrn, sample_g_inh, sample_g_exc):
+    """Tests the somatic conductances.
+
+    eq. 10 and 11 in the manuscript
+    """
+    phi_el = eq_phi(std_nrn.E_l, std_nrn.a, std_nrn.b)
+
+    # test for excitatory synapses
+    expected = std_nrn.g_exc * sample_r
+    expected[sample_r <= phi_el] = phi_el * std_nrn.g_exc
+    assert_allclose(sample_g_exc, expected)
+
+    # test for inhibitory synapses
+    expected = std_nrn.g_inh * sample_r * 0.95
+    expected[sample_r * 0.95 <= phi_el] = phi_el * std_nrn.g_inh
+    assert_allclose(sample_g_inh, expected)
+
+
+def test_rescale_v(sample_u, std_nrn):
+    """Tests the computation of the rescaled dendritic voltage.
+
+    Needed in the plasticity rule.
+    """
+    res = eq_rescale_v(sample_u, std_nrn.g_l, std_nrn.g_den, std_nrn.E_l)
+    expected = (
+        1.0
+        / (std_nrn.g_l + std_nrn.g_den)
+        * (std_nrn.g_l * std_nrn.E_l + std_nrn.g_den * sample_u)
+    )
+    assert_allclose(res, expected)
+
+
+def test_drbar_dt(sample_r, std_nrn):
+    """Tests low pass filtered rate."""
+    sample_rbar = sample_r * 1.2
+    res = eq_drbar_dt(sample_rbar, sample_r, std_nrn.g_l, std_nrn.g_den)
+    expected = (
+        -std_nrn.g_l * sample_rbar
+        + (std_nrn.g_l * std_nrn.g_den) / (std_nrn.g_l + std_nrn.g_den) * sample_r
+    )
+    assert_allclose(res, expected)
+
+
+def test_i_som(
+    sample_u, std_nrn, sample_g_exc, sample_g_inh, sample_W_som, sample_i_som
+):
+    """Tests the somatic input current without external teaching."""
+
+    expected = np.zeros(sample_u.shape)
+    for j, u in enumerate(sample_u):
+        for i, (g_exc, g_inh) in enumerate(zip(sample_g_exc, sample_g_inh)):
+            expected[j] += sample_W_som[j, i] * (
+                g_exc * (std_nrn.E_exc - u) + g_inh * (std_nrn.E_inh - u)
+            )
+
+    assert_allclose(sample_i_som, expected)
+
+
+def test_inp_cond(sample_inp, sample_g_exc_inp, sample_g_inh_inp, std_nrn):
+    """Test the target input synaptic conductances"""
+    # excitatory
+    expected = (
+        std_nrn.lam
+        / (1 - std_nrn.lam)
+        * (std_nrn.g_l + std_nrn.g_den)
+        * (std_nrn.E_inh - sample_inp)
+        / (std_nrn.E_inh - std_nrn.E_exc)
+    )
+    assert_allclose(sample_g_exc_inp, expected)
+    # inhibitory
+    expected = (
+        std_nrn.lam
+        / (1 - std_nrn.lam)
+        * (std_nrn.g_l + std_nrn.g_den)
+        * (sample_inp - std_nrn.E_exc)
+        / (std_nrn.E_inh - std_nrn.E_exc)
+    )
+    assert_allclose(sample_g_inh_inp, expected)
+
+
+def test_i_inp(sample_u, sample_g_exc_inp, sample_g_inh_inp, std_nrn, std_nw):
+    """Test the input currents of the target synapses.
+
+    This current is added to the visible somas only.
+    """
+    u = sample_u[: std_nw.num_vis]
+    res = eq_i_inp(sample_g_exc_inp, sample_g_inh_inp, u, std_nrn.E_exc, std_nrn.E_inh)
+
+    expected = np.zeros_like(u)
+    for i, (u_in, g_exc, g_inh) in enumerate(
+        zip(u, sample_g_exc_inp, sample_g_inh_inp)
+    ):
+        expected[i] = g_exc * (std_nrn.E_exc - u_in) + g_inh * (std_nrn.E_inh - u_in)
+    assert_allclose(res, expected)
+
+
+def test_dvdt(sample_v, sample_i_den, std_nrn):
+    """Tests the differential equation for the dendritiv voltage."""
+    res = eq_dvdt(sample_v, sample_i_den, std_nrn.C_v, std_nrn.g_l, std_nrn.E_l)
+    expected = np.zeros_like(sample_v)
+
+    for i, (v, i_den) in enumerate(zip(sample_v, sample_i_den)):
+        expected[i] = (-std_nrn.g_l * (v - std_nrn.E_l) + i_den) / std_nrn.C_v
+    assert_allclose(res, expected)
+
+
+def test_dudt(sample_u, sample_v, sample_i_som, std_nrn):
+    """Tests the diff. eq. for the somatic potential."""
+    res = eq_dudt(
+        sample_u,
+        sample_v,
+        sample_i_som,
+        std_nrn.C_u,
+        std_nrn.g_l,
+        std_nrn.g_den,
+        std_nrn.E_l,
+    )
+
+    expected = np.zeros_like(sample_v)
+
+    for i, (u, v, i_som) in enumerate(zip(sample_u, sample_v, sample_i_som)):
+        expected[i] = (
+            -std_nrn.g_l * (u - std_nrn.E_l) + std_nrn.g_den * (v - u) + i_som
+        ) / std_nrn.C_u
+    assert_allclose(res, expected)
+
+
+def test_dwdt(sample_u, sample_v, sample_r, std_nrn, std_nw):
+    """Tests plasticity rule for the dendritic weights."""
+    phi_u = eq_phi(sample_u, std_nrn.a, std_nrn.b)
+    v_rescaled = eq_rescale_v(sample_v, std_nrn.g_l, std_nrn.g_den, std_nrn.E_l)
+    phi_v = eq_phi(v_rescaled, std_nrn.a, std_nrn.b)
+    res = eq_dwdt(phi_u, phi_v, sample_r)
+
+    n_tot = std_nw.num_vis + std_nw.num_lat
+    expected = np.zeros((n_tot, n_tot))
+    for i in range(n_tot):
+        for j in range(n_tot):
+            expected[j, i] = (phi_u[j] - phi_v[j]) * sample_r[i]
+
+    assert_allclose(res, expected)
+
+
+def test_total_diff_eq(
+    sample_u,
+    sample_v,
+    sample_W_den,
+    sample_r_bar,
+    sample_r_delay_den,
+    sample_r_delay_exc,
+    sample_r_delay_inh,
+    sample_inp,
+    sample_W_som,
+    std_nrn,
+    sample_derivatives,
+):
+    """Tests the big function total_diff_eq."""
+    dudt, dvdt, dwdt, dr_bar_dt = total_diff_eq(
+        u=sample_u,
+        v=sample_v,
+        w_den=sample_W_den,
+        r_bar=sample_r_bar,
+        r_den=sample_r_delay_den,
+        r_exc=sample_r_delay_exc,
+        r_inh=sample_r_delay_inh,
+        u_inp=sample_inp,
+        w_som=sample_W_som,
+        C_u=std_nrn.C_u,
+        C_v=std_nrn.C_v,
+        g_l=std_nrn.g_l,
+        g_den=std_nrn.g_den,
+        g_exc_0=std_nrn.g_exc,
+        g_inh_0=std_nrn.g_inh,
+        E_l=std_nrn.E_l,
+        E_exc=std_nrn.E_exc,
+        E_inh=std_nrn.E_inh,
+        a=std_nrn.a,
+        b=std_nrn.b,
+        lam=std_nrn.lam,
+    )
+
+    expected = sample_derivatives
+
+    assert_allclose(dudt, expected[0])
+    assert_allclose(dvdt, expected[1])
+    assert_allclose(dwdt, expected[2])
+    assert_allclose(dr_bar_dt, expected[3])
