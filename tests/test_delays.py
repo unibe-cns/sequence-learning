82b5410c05086c828b707f1825ec831fe68df3fc
Ben von Huenerbein <ben.v.h@hotmail.com>
2024-10-23 11:17:23 +0200

    Create more concise delay tests

--- tests/test_delays.py
+++ tests/test_delays.py
@@ -23,36 +23,19 @@ def default_network_config():
     )
 
 
-# Test Dendritic delays
-def test_den_delay_basic(default_weight_config, default_network_config):
-    dw = DendriticWeights(default_weight_config)
-    _, delays = dw(default_network_config.num_vis, default_network_config.num_lat)
+@pytest.mark.parametrize("weight_class", [DendriticWeights, SomaticWeights])
+def test_delay_basic(weight_class, default_weight_config, default_network_config):
+    w = weight_class(default_weight_config)
+    _, delays = w(default_network_config.num_vis, default_network_config.num_lat)
     assert len(delays) == 63  # 50 + 13 = 63
     assert np.all((delays >= 5) & (delays <= 15))
     assert np.all(np.equal(np.mod(delays, 1), 0))
 
 
-def test_den_delay_consistency(default_weight_config, default_network_config):
-    dw1 = DendriticWeights(default_weight_config)
-    dw2 = DendriticWeights(default_weight_config)
-    _, delays1 = dw1(default_network_config.num_vis, default_network_config.num_lat)
-    _, delays2 = dw2(default_network_config.num_vis, default_network_config.num_lat)
-    np.testing.assert_allclose(delays1, delays2)
-
-
-# Test Somatic delays
-def test_som_delaybasic(default_weight_config, default_network_config):
-    sw = SomaticWeights(default_weight_config)
-    _, delays = sw(default_network_config.num_vis, default_network_config.num_lat)
-    assert isinstance(delays, np.ndarray)
-    assert len(delays) == 63  # 50 + 13 = 63
-    assert np.all((delays >= 5) & (delays <= 15))
-    assert np.all(np.equal(np.mod(delays, 1), 0))
-
-
-def test_som_matrix_consistency(default_weight_config, default_network_config):
-    sw1 = SomaticWeights(default_weight_config)
-    sw2 = SomaticWeights(default_weight_config)
-    _, delays1 = sw1(default_network_config.num_vis, default_network_config.num_lat)
-    _, delays2 = sw2(default_network_config.num_vis, default_network_config.num_lat)
+@pytest.mark.parametrize("weight_class", [DendriticWeights, SomaticWeights])
+def test_delay_consistency(weight_class, default_weight_config, default_network_config):
+    w1 = weight_class(default_weight_config)
+    w2 = weight_class(default_weight_config)
+    _, delays1 = w1(default_network_config.num_vis, default_network_config.num_lat)
+    _, delays2 = w2(default_network_config.num_vis, default_network_config.num_lat)
     np.testing.assert_allclose(delays1, delays2)
