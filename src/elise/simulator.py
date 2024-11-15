a5251f493fe3bdc2adfaf6e756089acd303e047b
Ben von Huenerbein <ben.v.h@hotmail.com>
2024-11-15 18:05:59 +0100

    Add simulator class

---
+++ src/elise/simulator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from elise.config import SimulationConfig
+from elise.logger import Logger
+from elise.model import Network
+from elise.optimizer import Optimizer
+
+
+class Simulation:
+    def __init__(
+        self,
+        simulation_params: SimulationConfig,
+        optimizer: Optimizer,
+        network: Network,
+        logger: Logger,
+    ):
+        # Params
+        self.dt = simulation_params.dt
+        self.training_cycles = simulation_params.training_cycles
+        self.replay_cycles = simulation_params.replay_cycles
+        self.validation_interval = simulation_params.validation_interval
+
+        self.network = network
+        self.logger = logger
+
+        # Optimizer
+        self.eta_lat = simulation_params.eta_lat / self.dt
+        self.eta_vis = simulation_params.eta_out / self.dt
+        self.optimizer_lat = optimizer(self.eta_lat)
+        self.optimizer_out = optimizer(self.eta_vis)
+
+        self.network.prepare_for_simulation(self.dt)
