23845eda38eebe38846b464896840fbabccb0ad5
Benano <ben.v.h@hotmail.com>
2024-12-12 10:49:18 +0100

    Feature simulation (#64)
    * Move rate buffer from neurons into network class
    
    * Implement first version simulation step
    
    * Move Neuron class into Network class
    
    * Move inhibitory delay to somatic weight class
    
    * Add incoming rates as attribute of network
    
    * Improved buffer depth calc
    
    * Implement working update step
    
    * Pass optimizers to network class during prep
    
    * Correct network update functions
    
    * Correct interneuron delay creation
    
    * Correct typing in Weights abstract class
    
    * Re-Correct interneuron delay creation
    
    * Remove Weight Class from model
    
    * Fix weight class import in model
    
    * Change net.simulation_step to net.__call__
    
    * trigger workflow

--- src/elise/model.py
+++ src/elise/model.py
@@ -1,298 +1,131 @@
 #!/usr/bin/env python3
-from abc import ABC, abstractmethod
 from typing import Tuple
 
 import numba
 import numpy as np
 import numpy.typing as npt
 
-from .config import NetworkConfig, NeuronConfig, WeightConfig
+from .config import NetworkConfig, NeuronConfig
 from .rate_buffer import Buffer
+from .weights import DendriticWeights, SomaticWeights
 
 # disable numba jit for debugging etc.
 numba.config.DISABLE_JIT = False
-
-
-class Weights(ABC):
-    """
-    Base class for weight matrices in neural networks.
-    """
-
-    @abstractmethod
-    def __init__(self, weight_params: WeightConfig):
-        pass
-
-    def __call__(self, num_vis: int, num_lat: int) -> Tuple[npt.NDArray, npt.NDArray]:
-        """
-        Create weight matrix.
-
-        :param num_vis: Number of visible neurons.
-        :type num_vis: int
-        :param num_lat: Number of lateral neurons.
-        :type num_lat: int
-        :return: A tuple containing the created weight matrix and associated delays.
-        :rtype: Tuple[npt.NDArray]
-        """
-        weight_matrix = self._create_weight_matrix(num_vis, num_lat)
-        delays = self._create_delays(num_vis, num_lat)
-        return weight_matrix, delays
-
-    def _create_delays(self, num_vis: int, num_lat: int) -> Tuple[npt.NDArray]:
-        "Create the delays associated with the weight matrix"
-        """
-        :param num_vis: Number of visible neurons.
-        :type num_vis: int
-        :param num_lat: Number of lateral neurons.
-        :type num_lat: int
-        :return: A tuple containing the created delays.
-        :rtype: Tuple[npt.NDArray]
-        """
-
-        num_total = num_vis + num_lat
-        d_min = self.delays[0]
-        d_max = self.delays[1]
-        delays = self.rng_d.integers(d_min, d_max, num_total)
-
-        return delays
-
-    @abstractmethod
-    def _create_weight_matrix(self, num_vis: int, num_lat: int):
-        pass
-
-
-class DendriticWeights(Weights):
-    """
-    Class for creating dendritic weight matrices.
-
-    This class extends the base Weights class and provides functionality to create
-    dendritic weight matrices based on the given configuration.
-
-    :param weight_params: Configuration object containing weight parameters.
-    :type weight_params: WeightConfig
-    """
-
-    def __init__(self, weight_params: WeightConfig):
-        """
-        Initialize the DendriticWeights object.
-
-        :param weight_params: Configuration object containing weight parameters.
-        :type weight_params: WeightConfig
-        """
-        super().__init__(weight_params)
-        self.delays = weight_params.d_den
-        self.W_vis_vis = weight_params.W_vis_vis
-        self.W_vis_lat = weight_params.W_vis_lat
-        self.W_lat_vis = weight_params.W_lat_vis
-        self.W_lat_lat = weight_params.W_lat_lat
-        self.d_den = weight_params.d_den
-        self.rng_w = np.random.default_rng(seed=weight_params.w_den_seed)
-        self.rng_d = np.random.default_rng(seed=weight_params.d_den_seed)
-
-    def _create_weight_matrix(self, num_vis: int, num_lat: int) -> Tuple[npt.NDArray]:
-        """
-        Create the dendritic weight matrix.
-
-        This method initializes a weight matrix and fills it with random values
-        based on the configuration parameters.
-
-        :param num_vis: Number of visible neurons.
-        :type num_vis: int
-        :param num_lat: Number of lateral neurons.
-        :type num_lat: int
-        :return: The created weight matrix.
-        :rtype: Tuple[npt.NDArray]
-        """
-        # Initialize weight matrix
-        weights = np.zeros((num_vis + num_lat, num_vis + num_lat))
-        weights[num_vis:, num_vis:] = self.rng_w.uniform(
-            self.W_lat_lat[0], self.W_lat_lat[1], (num_lat, num_lat)
-        )  # Lat to Lat
-        weights[num_vis:, :num_vis] = self.rng_w.uniform(
-            self.W_lat_vis[0], self.W_lat_vis[1], (num_lat, num_vis)
-        )  # Lat to Vis
-        weights[:num_vis, num_vis:] = self.rng_w.uniform(
-            self.W_vis_lat[0], self.W_vis_lat[1], (num_vis, num_lat)
-        )  # Vis to Lat
-        weights[:num_vis, :num_vis:] = self.rng_w.uniform(
-            self.W_vis_vis[0], self.W_vis_vis[1], (num_vis, num_vis)
-        )  # Vis to Vis
-
-        # remove self connections
-        np.fill_diagonal(weights, 0)
-
-        return weights
-
-
-class SomaticWeights(Weights):
-    """
-    Class for creating somatic weight matrices.
-
-    This class extends the base Weights class and provides functionality to create
-    somatic weight matrices based on probabilistic connection rules.
-
-    :param weight_params: Configuration object containing weight parameters.
-    :type weight_params: WeightConfig
-    """
-
-    def __init__(self, weight_params: WeightConfig):
-        """
-        Initialize the SomaticWeights object.
-
-        :param weight_params: Configuration object containing weight parameters.
-        :type weight_params: WeightConfig
-        """
-        super().__init__(weight_params)
-        self.delays = weight_params.d_som
-        self.p = weight_params.p
-        self.q = weight_params.q
-        self.p0 = weight_params.p0
-        self.p_first = 1 - self.p0
-        self.rng_w = np.random.default_rng(seed=weight_params.w_som_seed)
-        self.rng_d = np.random.default_rng(seed=weight_params.d_som_seed)
-
-    def _create_weight_matrix(self, num_vis: int, num_lat: int) -> Tuple[npt.NDArray]:
-        """
-        Create the somatic weight matrix based on probabilistic connection rules.
-
-        This method implements a complex algorithm to create connections between
-        neurons based on various probabilities and rules.
-
-        :param num_vis: Number of visible neurons.
-        :type num_vis: int
-        :param num_lat: Number of lateral neurons.
-        :type num_lat: int
-        :return: The created weight matrix.
-        :rtype: Tuple[npt.NDArray]
-        """
-        num_total = num_vis + num_lat
-        neurons_outgoing = np.arange(num_total)
-        neurons_incoming = np.arange(num_vis, num_total)
-        weight_matrix = np.zeros((num_total, num_total))
-
-        # Incoming connections
-        connections_in = np.zeros(num_total)
-        connections_in[:num_vis] = 1
-
-        # Outgoing connections
-        connections_out = np.zeros(num_total)
-
-        # Neurons that can make a connection
-        neurons_unspent = np.arange(num_total)
-        neurons_looking = neurons_outgoing[connections_in > 0]
-
-        while len(neurons_looking) > 0:
-            for idx_pre in neurons_looking:
-                while np.isin(idx_pre, neurons_looking):
-                    # Probability for forming connection
-                    if connections_out[idx_pre] == 0:
-                        prob_out = 1 - self.p0
-                    else:
-                        prob_out = np.power(self.p, connections_out[idx_pre])
-
-                    # Test for formation
-                    formation = self.rng_w.binomial(1, prob_out)
-                    if not formation:
-                        # Remove neuron pre from list of unspent neurons
-                        neurons_unspent = neurons_unspent[neurons_unspent != idx_pre]
-                        neurons_looking = np.delete(neurons_looking, 0)
-                    else:
-                        # Exclude connections to self and to neurons already connected
-                        possible_post = neurons_incoming[neurons_incoming != idx_pre]
-                        neurons_connected = np.where(weight_matrix[:, idx_pre] == 1)[0]
-                        possible_post = np.setdiff1d(possible_post, neurons_connected)
-
-                        formed = 0
-                        while not formed:
-                            post_idx = self.rng_w.choice(possible_post)
-                            prob_in = np.power(self.q, connections_in[post_idx])
-                            accept = self.rng_w.binomial(1, prob_in)
-                            if accept:
-                                # Add connection to matrix
-                                weight_matrix[post_idx, idx_pre] = 1
-                                connections_out[idx_pre] += 1
-                                connections_in[post_idx] += 1
-                                if np.isin(post_idx, neurons_unspent):
-                                    neurons_unspent = neurons_unspent[
-                                        neurons_unspent != post_idx
-                                    ]
-                                    neurons_looking = np.append(
-                                        neurons_looking, post_idx
-                                    )
-                                formed = 1
-                            else:
-                                continue
-
-        if np.sum(weight_matrix) != np.sum(connections_in) - num_vis:
-            print("Problem with total connections")
-
-        return weight_matrix
-
-
-class Neurons:
-    def __init__(
-        self, neuron_params: NeuronConfig, num_neurons: int, rate_buffer: Buffer
-    ):
-        # Parameters
-        self.C_v = neuron_params.C_v
-        self.C_u = neuron_params.C_u
-        self.E_l = neuron_params.E_l
-        self.E_exc = neuron_params.E_exc
-        self.E_inh = neuron_params.E_inh
-        self.g_l = neuron_params.g_l
-        self.g_den = neuron_params.g_den
-        self.g_exc_0 = neuron_params.g_exc_0
-        self.g_inh_0 = neuron_params.g_inh_0
-        self.a = neuron_params.a
-        self.b = neuron_params.b
-        self.d_den = neuron_params.d_den
-        self.d_som = neuron_params.d_som
-        self.d_int = neuron_params.d_int
-        self.lam = neuron_params.lam
-
-        # Dynamical variables
-        self.v = np.ones(num_neurons) * self.E_l
-        self.u = np.ones(num_neurons) * self.E_l
-        self.r_rest = eq_phi(self.E_l, self.a, self.b)
-        self.r_bar = np.ones(num_neurons) * self.r_rest
-        self.r = np.ones(num_neurons) * self.r_rest
 
 
 class Network:
     def __init__(
         self,
         network_params: NetworkConfig,
-        weight_params: WeightConfig,
         neuron_params: NeuronConfig,
         dendritic_weights: DendriticWeights,
         somatic_weights: SomaticWeights,
-        neurons: Neurons,
         rate_buffer: Buffer,
     ):
+        self.neuron_params = neuron_params
         self.num_lat = network_params.num_lat
         self.num_vis = network_params.num_vis
         self.num_all = self.num_lat + self.num_vis
-        self.dendritic_weights, self.delays_den = dendritic_weights(
-            weight_params, self.num_lat, self.num_vis
+
+        self.dendritic_weights, self.dendritic_delays = dendritic_weights(
+            self.num_lat, self.num_vis
         )
-        self.somatic_weights, self.delays_som = somatic_weights(
-            weight_params, self.num_vis, self.num_lat
+        self.somatic_weights, self.somatic_delays = somatic_weights(
+            self.num_vis, self.num_lat
         )
-        self.neurons = neurons(neuron_params, self.num_all, rate_buffer)
+        self.interneuron_delays = somatic_weights.create_interneuron_delays()
 
-    def simulation_step(self, dt):
-        # Compute r_den
-        # Compute r_exc
-        # Compute r_inh
+        self.r_rest = eq_phi(
+            self.neuron_params.E_l, self.neuron_params.a, self.neuron_params.b
+        )
 
-        # Run differential equation
-        # Update variables u, v, w, r_bar
-        # Compute new r
-        # Update buffer
-        pass
+        # Dynamical variables
+        self.v = np.ones(self.num_all) * self.neuron_params.E_l
+        self.u = np.ones(self.num_all) * self.neuron_params.E_l
+        self.r_bar = np.ones(self.num_all) * self.r_rest
+        self.r = np.ones(self.num_all) * self.r_rest
+        self.r_den = np.ones(self.num_all) * self.r_rest
+        self.r_exc = np.ones(self.num_all) * self.r_rest
+        self.r_inh = np.ones(self.num_all) * self.r_rest
 
-    def update(self):
-        # Implement update logic here
-        pass
+        self.dt = None
+
+    def _compute_buffer_depth(self, dt):
+        max_buffer_ms = max(max(self.dendritic_delays), max(self.interneuron_delays))
+        buffer_depth = int(max_buffer_ms / dt)
+
+        return buffer_depth
+
+    def prepare_for_simulation(self, dt, optimizer_vis, optimizer_lat):
+        # Configure rate buffer for simulation
+        self.rate_buffer = Buffer(
+            self.num_all, self._compute_buffer_depth(dt), self.r_rest
+        )
+        self.dt = dt
+        self.optimizer_vis = optimizer_vis
+        self.optimizer_lat = optimizer_lat
+
+    def _compute_update(self, u_inp):
+        # Compute delayed rates
+        self.r_den = self.rate_buffer.get(self.dendritic_delays)
+        self.r_exc = self.rate_buffer.get(self.somatic_delays)
+        self.r_inh = self.rate_buffer.get(self.interneuron_delays)
+
+        dudt, dvdt, dwdt, dr_bar_dt = total_diff_eq(
+            u=self.u,
+            v=self.v,
+            w_den=self.dendritic_weights,
+            r_bar=self.r_bar,
+            r_den=self.r_den,
+            r_exc=self.r_exc,
+            r_inh=self.r_inh,
+            u_inp=u_inp,
+            w_som=self.somatic_weights,
+            C_v=self.neuron_params.C_v,
+            C_u=self.neuron_params.C_u,
+            E_l=self.neuron_params.E_l,
+            E_exc=self.neuron_params.E_exc,
+            E_inh=self.neuron_params.E_inh,
+            g_l=self.neuron_params.g_l,
+            g_den=self.neuron_params.g_den,
+            g_exc_0=self.neuron_params.g_exc_0,
+            g_inh_0=self.neuron_params.g_inh_0,
+            a=self.neuron_params.a,
+            b=self.neuron_params.b,
+            lam=self.neuron_params.lam,
+        )
+
+        return dudt, dvdt, dwdt, dr_bar_dt
+
+    def _update_weights(self, dwdt):
+        # Update lat
+        w_lat = self.dendritic_weights[self.num_vis :, :]
+        w_vis = self.dendritic_weights[: self.num_vis, :]
+        dwdt_lat = dwdt[self.num_vis :, :]
+        dwdt_vis = dwdt[: self.num_vis, :]
+
+        # Apply optimizers
+        w_lat_new = self.optimizer_lat.update(w_lat, dwdt_lat)
+        w_vis_new = self.optimizer_vis.update(w_vis, dwdt_vis)
+
+        self.dendritic_weights[self.num_vis :, :] = w_lat_new
+        self.dendritic_weights[: self.num_vis, :] = w_vis_new
+
+    def _update_dyanmic_variables(self, dudt, dvdt, dr_bar_dt):
+        self.u += dudt * self.dt
+        self.v += dvdt * self.dt
+        self.r_bar += dr_bar_dt * self.dt
+
+    def _update_rates_and_buffer(self):
+        new_r = eq_phi(self.u, self.neuron_params.a, self.neuron_params.b)
+        self.r = new_r
+        self.rate_buffer.roll(new_r)
+
+    def __call__(self, u_inp):
+        dudt, dvdt, dwdt, dr_bar_dt = self._compute_update(u_inp)
+        self._update_dyanmic_variables(dudt, dvdt, dr_bar_dt)
+        self._update_weights(dwdt)
+        self._update_rates_and_buffer()
 
 
 #####################################
