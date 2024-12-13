1efecf57501315e048e7177aa9c09f11d36b7eb6
Timo Gierlich <timo@eltima-electronic.de>
2024-12-13 13:48:25 +0100

    Add functions to evaluate the model performance
    eq. 22 - 24 in the script

---
+++ src/elise/stats.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+Functions for evaluating the model performance.
+"""
+from typing import Callable
+
+import numpy as np
+import numpy.typing as npt
+from numpy.lib.stride_tricks import sliding_window_view
+from scipy.stats import pearsonr
+
+
+def window_slider(
+    data: npt.NDArray, target: npt.NDArray, func: Callable[[npt.NDArray], float]
+):
+    """
+    Apply a sliding window operation on data using a given function.
+
+    :param data: Input data array
+    :type data: npt.NDArray
+    :param target: Target array defining the window shape
+    :type target: npt.NDArray
+    :param func: Function to apply on each window
+    :type func: Callable[[npt.NDArray, npt.NDArray], float]
+    :return: Array of results from applying func on each window
+    :rtype: npt.NDArray
+    """
+    windows = sliding_window_view(data, target.shape).squeeze()
+    return np.array([func(chunk, target) for chunk in windows])
+
+
+def pearson_coef(data: npt.NDArray, target: npt.NDArray):
+    """
+    Calculate the mean Pearson correlation coefficient between data and target.
+
+    :param data: Input data array
+    :type data: npt.NDArray
+    :param target: Target array
+    :type target: npt.NDArray
+    :return: Mean Pearson correlation coefficient
+    :rtype: float
+    """
+    p_coefs = pearsonr(data, target, axis=0).statistic
+    return np.mean(p_coefs)
+
+
+def mse(data: npt.NDArray, target: npt.NDArray):
+    """
+    Calculate the Mean Squared Error between data and target.
+
+    :param data: Input data array
+    :type data: npt.NDArray
+    :param target: Target array
+    :type target: npt.NDArray
+    :return: Mean Squared Error
+    :rtype: float
+    """
+    return np.mean((data - target) ** 2)
