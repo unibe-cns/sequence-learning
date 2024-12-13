eec47a11b943ad070e0ab4b8306ba7c4e6f872c5
Ben von Huenerbein <ben.v.h@hotmail.com>
2024-12-13 14:12:37 +0100

    Fix dataloader interface

--- src/elise/simulator.py
+++ src/elise/simulator.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+from tqdm import tqdm
+
 from elise.config import SimulationConfig
-from elise.data import DataLoader
+from elise.data import Dataloader
 from elise.logger import Logger
 from elise.model import Network
 from elise.optimizer import Optimizer
@@ -14,12 +16,13 @@ class Simulation:
         optimizer: Optimizer,
         network: Network,
         logger: Logger,
-        dataloader: DataLoader,
+        dataloader: Dataloader,
     ):
         # Params
         self.dt = simulation_params.dt
         self.training_cycles = simulation_params.training_cycles
         self.replay_cycles = simulation_params.replay_cycles
+        self.validation_cycles = simulation_params.validation_cycles
         self.validation_interval = simulation_params.validation_interval
 
         self.network = network
@@ -30,14 +33,16 @@ class Simulation:
         self.eta_lat = simulation_params.eta_lat / self.dt
         self.eta_vis = simulation_params.eta_out / self.dt
         self.optimizer_lat = optimizer(self.eta_lat)
-        self.optimizer_out = optimizer(self.eta_vis)
+        self.optimizer_vis = optimizer(self.eta_vis)
 
         self.network.prepare_for_simulation(
             self.dt, self.optimizer_vis, self.optimizer_lat
         )
 
     def epoch(self, teacher=None):
-        for u_tgt in self.dataloader:
+        start = 0
+        stop = self.dataloader.duration
+        for t, u_tgt in self.dataloader.iter(start, stop, self.dt):
             if teacher:
                 self.network(u_inp=u_tgt)
             elif not teacher:
@@ -48,16 +53,19 @@ class Simulation:
             # Intra-epoch logging
 
     def training(self):
-        for i in range(self.training_cycles):
+        print("Training")
+        for t in tqdm(range(self.training_cycles)):
             self.epoch(teacher=True)
 
-            if i % self.validation_interval == 0:
-                self.epoch(teacher=False)
+            if t % self.validation_interval == 0:
+                for v in range(self.validation_cycles):
+                    self.epoch(teacher=False)
 
-                # Validation logging
+                    # Validation logging
 
     def replay(self):
-        for i in range(self.replay_cycles):
+        print("Replay")
+        for r in tqdm(range(self.replay_cycles)):
             self.epoch(teacher=False)
 
             # Replay logging
