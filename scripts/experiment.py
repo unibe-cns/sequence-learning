9c433510750fbe86e83a461cee7bf8a9a753d162
Timo Gierlich <timo@eltima-electronic.de>
2024-12-18 14:37:02 +0100

    Add performance measures in validation (#99)
    - both MSE and pearson corr coef
    - both on u and r

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -10,9 +10,10 @@ from tqdm import tqdm
 from elise.config import FullConfig
 from elise.data import Dataloader, MultiHotPattern
 from elise.logger import Logger
-from elise.model import Network
+from elise.model import Network, eq_phi
 from elise.optimizer import SimpleUpdater
 from elise.rate_buffer import Buffer
+from elise.stats import mse, pearson_coef, window_slider
 from elise.weights import DendriticWeights, SomaticWeights
 
 # Config
@@ -57,6 +58,22 @@ def to_biounits(x):
     )  # TODO: include the 20. as a parameter in the config.
 
 
+def eq_phi_(x):
+    return eq_phi(
+        x,
+        neuron_params.a,
+        neuron_params.b,
+    )
+
+
+def calc_val_mse(data, target):
+    return np.min(window_slider(data, target, mse))
+
+
+def calc_val_pearson(data, target):
+    return np.max(window_slider(data, target, pearson_coef))
+
+
 # Pattern & Dataloader
 pat = np.loadtxt("fuer_elise_short.txt", delimiter=",", skiprows=1).astype(int)
 pat_duration = 250.0
@@ -64,10 +81,11 @@ pattern_width = network_params.num_vis
 fuer_elise = MultiHotPattern(pat, pat_duration, pattern_width)
 dataloader = Dataloader(fuer_elise, pre_transforms=[to_biounits])
 
-full_pattern = dataloader.get_full_pattern(dt)
+full_pattern_u = dataloader.get_full_pattern(dt)
+full_pattern_r = eq_phi_(full_pattern_u)
 
 training_duration = 10 * pat_duration
-validation_duration = 0 * pat_duration
+validation_duration = 1.5 * pat_duration
 replay_duration = 6 * pat_duration
 num_epochs = 10
 
@@ -79,9 +97,13 @@ target = []
 r_bar = []
 weights = defaultdict(list)
 
+all_mses = defaultdict(list)
+all_corr_coefs = defaultdict(list)
+
 weight_types = ["vis_vis", "vis_lat", "lat_lat", "lat_vis"]
 
 for epoch in tqdm(range(num_epochs)):
+    # TRAINING ITERATION
     for _, u_tgt in dataloader.iter(0, training_duration, dt):
         network(u_inp=u_tgt)
 
@@ -91,16 +113,28 @@ for epoch in tqdm(range(num_epochs)):
 
         target.append(u_tgt)
 
+    # VALIDATION ITERATION
+    val_output = []
     for _, u_tgt in dataloader.iter(0, validation_duration, dt):
         network(u_inp=None)
 
         output = network.get_output()
 
         simulation_output.append(output)
+        val_output.append(output)
 
         target.append(u_tgt)
 
+    val_output_u = np.array(val_output)
+    val_output_r = eq_phi_(val_output_u)
 
+    all_mses["u"].append(calc_val_mse(val_output_u, full_pattern_u))
+    all_mses["r"].append(calc_val_mse(val_output_r, full_pattern_r))
+
+    all_corr_coefs["u"].append(calc_val_pearson(val_output_u, full_pattern_u))
+    all_corr_coefs["r"].append(calc_val_pearson(val_output_r, full_pattern_r))
+
+# FINAL REPLAY
 for _, u_tgt in dataloader.iter(0, replay_duration, dt):
     network(u_inp=None)
 
@@ -113,7 +147,19 @@ for _, u_tgt in dataloader.iter(0, replay_duration, dt):
 simulation_output = np.array(simulation_output)
 target = np.array(target)
 
-plot_activity(simulation_output, target, full_pattern, replay_duration, dt)
+plot_activity(simulation_output, target, full_pattern_u, replay_duration, dt)
 plot_weights(network)
+
+fig, ax = plt.subplots(4, 1, sharex=True)
+ax[0].set_title("validation.")
+ax[0].plot(all_mses["u"])
+ax[1].plot(all_mses["r"])
+ax[0].set_ylabel("MSE on u")
+ax[1].set_ylabel("MSE on r")
+ax[2].plot(all_corr_coefs["u"])
+ax[3].plot(all_corr_coefs["r"])
+ax[2].set_ylabel("pearson on u")
+ax[3].set_ylabel("pearson on r")
+ax[3].set_xlabel("epochs")
 
 plt.show()
