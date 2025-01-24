5f7ce02e288d99c31e423b17857b5c42c0438b92
Ben von Huenerbein <ben.v.h@hotmail.com>
2025-01-24 13:43:03 +0100

    Add get_epoch functionality

--- src/elise/tracker.py
+++ src/elise/tracker.py
@@ -10,7 +10,11 @@ from .model import Network
 
 class Tracker:
     def __init__(
-        self, network: Network, variables: list[tuple[str, str]], track_step: int
+        self,
+        network: Network,
+        variables: list[tuple[str, str]],
+        track_step: int,
+        epoch_duration: int,
     ):
         if not all(
             isinstance(var, (list, tuple)) and len(var) == 2 for var in variables
@@ -23,6 +27,8 @@ class Tracker:
         self.track_step = track_step
         self.track_counter = 0
         self.track_dict = defaultdict(list)
+        self.epoch_duration = epoch_duration
+        self.effective_epoch_duration = epoch_duration // track_step
 
     def _get_variable_value(self, name: str, view: str):
         try:
@@ -53,9 +59,24 @@ class Tracker:
     def __getitem__(self, item):
         return np.array(self.track_dict[item])
 
-    def __repr__(self):
-        return (
-            f"Tracker({list(self.track_dict.keys())}, "
-            f"length={self.track_counter}, "
-            f"track_step={self.track_step})"
-        )
+    def get_epochs(self, item, epoch_nums):
+        if isinstance(epoch_nums, (int, np.integer)):
+            epoch_nums = [epoch_nums]
+
+        epochs = []
+        for epoch_num in epoch_nums:
+            epoch_slice = np.s_[
+                epoch_num
+                * self.effective_epoch_duration : (epoch_num + 1)
+                * self.effective_epoch_duration
+            ]
+            epochs.append(np.array(self.track_dict[item])[epoch_slice])
+
+        return np.concatenate(epochs)
+
+        def __repr__(self):
+            return (
+                f"Tracker({list(self.track_dict.keys())}, "
+                f"length={self.track_counter}, "
+                f"track_step={self.track_step})"
+            )
