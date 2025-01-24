fc1631741813a71df1591f5f95b14a451a2d1802
Ben von Huenerbein <ben.v.h@hotmail.com>
2025-01-24 13:42:36 +0100

    Add better warning messages to tracker

--- src/elise/tracker.py
+++ src/elise/tracker.py
@@ -9,26 +9,37 @@ from .model import Network
 
 
 class Tracker:
-    def __init__(self, network: Network, variables: list, track_step: int = 1):
+    def __init__(
+        self, network: Network, variables: list[tuple[str, str]], track_step: int
+    ):
+        if not all(
+            isinstance(var, (list, tuple)) and len(var) == 2 for var in variables
+        ):
+            raise ValueError(
+                "Variables must be list of lists with two elements (name, view)."
+            )
         self.network = network
         self.variables = variables
         self.track_step = track_step
         self.track_counter = 0
         self.track_dict = defaultdict(list)
 
-    def track(self):
+    def _get_variable_value(self, name: str, view: str):
+        try:
+            return self.network.get_val(name, view)
+        except Exception as e:
+            print(f"Error retrieving {name} ({view}): {str(e)}")
+            return None
+
+    def track(self, time):
+        self.track_dict["time"].append(time)
         self.track_counter += 1
         if self.track_counter % self.track_step == 0:
-            try:
-                for name, view in self.variables:
-                    key = f"{name}_{view}"
-                    val = self.network.get_val(name, view)
+            for name, view in self.variables:
+                key = f"{name}_{view}"
+                val = self._get_variable_value(name, view)
+                if val is not None:  # Only store valid results
                     self.track_dict[key].append(val)
-
-            except Exception as e:
-                print(f"Error during tracking: {str(e)}")
-        else:
-            pass
 
     def save(self, path):
         with open(path, "wb") as f:
